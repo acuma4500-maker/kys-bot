87dcacafd39c3a57fee4faea0b6310b98d62e9cb
acuma4500-maker <acuma4500@gmail.com>
2026-07-17 10:32:18 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,3 @@
 export interface Env {
   BOT_TOKEN: string;
-  CHAT_ID: string;
-  TELEGRAM_SECRET?: string;
-  CHECK_URL: string;
-}
-
-export function getConfig(env: Env) {
-  return {
-    botToken: env.BOT_TOKEN,
-    chatId: env.CHAT_ID,
-    telegramSecret: env.TELEGRAM_SECRET || "",
-    checkUrl: env.CHECK_URL,
-  };
 }
