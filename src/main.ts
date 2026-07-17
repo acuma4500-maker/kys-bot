cb5c31cc5f375c52d07ea70256e9985e05cf38cd
acuma4500-maker <acuma4500@gmail.com>
2026-07-17 16:44:47 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,17 @@
 import { Bot, webhookCallback } from "grammy";
 
-import { registerStart } from "./commands/start";
-import { registerCallbacks } from "./callbacks";
-
 export interface Env {
   BOT_TOKEN: string;
 }
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-
     const bot = new Bot(env.BOT_TOKEN);
 
-    // Komutları yükle
-    registerStart(bot);
+    bot.command("start", async (ctx) => {
+      await ctx.reply("Merhaba");
+    });
 
-    // Butonları yükle
-    registerCallbacks(bot);
-
-    // Telegram webhook
     return webhookCallback(bot, "cloudflare-mod")(request);
-
   },
 };
