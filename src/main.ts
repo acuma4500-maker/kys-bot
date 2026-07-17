348d90e838ce93d0c6902bdec1c5f871cc6b83f8
acuma4500-maker <acuma4500@gmail.com>
2026-07-17 18:17:55 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,5 @@
-import { Bot, webhookCallback } from "grammy";
-
-export interface Env {
-  BOT_TOKEN: string;
-}
-
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    const bot = new Bot(env.BOT_TOKEN);
-
-    bot.command("start", async (ctx) => {
-      await ctx.reply("Merhaba");
-    });
-
-    return webhookCallback(bot, "cloudflare-mod")(request);
+  async fetch() {
+    return new Response("KYS OK");
   },
 };
