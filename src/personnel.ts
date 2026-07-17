fcfc53db73a29657a9ebdb644c7288c1c544a1a2
acuma4500-maker <acuma4500@gmail.com>
2026-07-17 12:10:44 +0300

    Update personnel.ts

--- src/personnel.ts
+++ src/personnel.ts
@@ -1,20 +1,61 @@
-export interface Person {
-  id: number;
-  name: string;
-  fixedTask?: "ODA1" | "YAZHANE";
-}
+import { Person } from "./types";
 
 export const PERSONNEL: Person[] = [
-  { id: 1, name: "Ahmet", fixedTask: "ODA1" },
-  { id: 2, name: "Mertali", fixedTask: "YAZHANE" },
-
-  { id: 3, name: "Ali" },
-  { id: 4, name: "Mustafa" },
-  { id: 5, name: "Cuma" },
-  { id: 6, name: "Hasan" },
-  { id: 7, name: "Hüseyin" },
-  { id: 8, name: "Yusuf" },
-  { id: 9, name: "Samet" },
-  { id: 10, name: "Taner" },
-  { id: 11, name: "Mert" },
+  {
+    id: 1,
+    name: "Ahmet",
+    fixedTask: "ODA1",
+    active: true,
+  },
+  {
+    id: 2,
+    name: "Mertali",
+    fixedTask: "YAZIHANE",
+    active: true,
+  },
+  {
+    id: 3,
+    name: "Ali",
+    active: true,
+  },
+  {
+    id: 4,
+    name: "Mustafa",
+    active: true,
+  },
+  {
+    id: 5,
+    name: "Cuma",
+    active: true,
+  },
+  {
+    id: 6,
+    name: "Hasan",
+    active: true,
+  },
+  {
+    id: 7,
+    name: "Hüseyin",
+    active: true,
+  },
+  {
+    id: 8,
+    name: "Yusuf",
+    active: true,
+  },
+  {
+    id: 9,
+    name: "Samet",
+    active: true,
+  },
+  {
+    id: 10,
+    name: "Taner",
+    active: true,
+  },
+  {
+    id: 11,
+    name: "Mert",
+    active: true,
+  },
 ];
